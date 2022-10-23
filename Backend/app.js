8ab0372f0b3b7359362b258d0685fc7bbc4ee535
Jessica Anna James <jessannajames@gmail.com>
2022-10-23 15:39:33 +0300

    added comment lines

--- Backend/app.js
+++ Backend/app.js
@@ -1,13 +1,13 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const router = require('./routes/Food_routes');
-const cors = require('cors');
-const app = express();
+const express = require('express');  //getting all the functionalities of express
+const mongoose = require('mongoose'); //getting all the functionalities of mongoose ( used to connect to the database)
+const router = require('./routes/Food_routes'); //get the CRUD routes
+const cors = require('cors'); // allows us to make requests from one website to another website which is usually prohibited accoridng to the SOP
+const app = express(); // variable app recieves all the functionalities of express library
 
 // Middlewares
 app.use(express.json());
 app.use(cors());
-app.use("/food", router); // localhost:5000/books
+app.use("/food", router); // localhost:5000/food
 
 mongoose
   .connect("mongodb+srv://dbuser:WxwAFpW3ff85ipi@cluster0.zp619fg.mongodb.net/?retryWrites=true&w=majority")
