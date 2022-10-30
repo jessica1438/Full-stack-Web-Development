d37c21ea47d3b012737fbd1d5c26b7442f455535
Jessica Anna James <jessannajames@gmail.com>
2022-10-30 16:23:31 +0300

    Added all neccesary changes to host the backend on Heroku

--- Backend/app.js
+++ Backend/app.js
@@ -1,16 +1,21 @@
+require("dotenv").config();
 const express = require('express');  //getting all the functionalities of express
 const mongoose = require('mongoose'); //getting all the functionalities of mongoose ( used to connect to the database)
 const router = require('./routes/Food_routes'); //get the CRUD routes
 const cors = require('cors'); // allows us to make requests from one website to another website which is usually prohibited accoridng to the SOP
 const app = express(); // variable app recieves all the functionalities of express library
 
+
+const conn = process.env.DATABASE
+
+const port = process.env.PORT || 5000;
 // Middlewares
 app.use(express.json());
 app.use(cors());
 app.use("/showRecipe", router); // localhost:5000/food
 
 mongoose
-  .connect("mongodb+srv://dbuser:WxwAFpW3ff85ipi@cluster0.zp619fg.mongodb.net/?retryWrites=true&w=majority")
+  .connect(conn,{useNewUrlParser:true,useUnifiedTopology:true})
   .then(() => console.log("Connected To Database"))
-  .then(() => {app.listen(5000);})
+  .then(() => {app.listen(port);})
   .catch((err) => console.log(err));
