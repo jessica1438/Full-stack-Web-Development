8df419fd9929ab7acba6dec078544f6890241a66
Jessica Anna James <jessannajames@gmail.com>
2022-10-31 16:15:41 +0300

    Update Header.js

--- Frontend/src/components/Header.js
+++ Frontend/src/components/Header.js
@@ -6,7 +6,7 @@ const Header = () => {
   const [value, setValue] = useState();
   return (
     <div>
-      <AppBar sx={{ backgroundColor: "#232F3D" }} position="sticky">
+      <AppBar sx={{ backgroundColor: "#9900CC" }} position="sticky">
         <Toolbar>
           <NavLink to="/" style={{ color: "white" }}>
             <Typography>
