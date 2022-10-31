ce2917e0cea091993667d5b0e0c6407832d7cbcf
Jessica Anna James <jessannajames@gmail.com>
2022-10-31 16:13:34 +0300

    Update Home.js

--- Frontend/src/components/Home.js
+++ Frontend/src/components/Home.js
@@ -7,7 +7,7 @@ const Home = () => {
     <div>
       <Box display="flex" flexDirection="column" alignItems="center">
         <Button
-          LinkComponent={Link} to="/showRecipe" sx={{ marginTop: 15, background: "orangered" }} variant="contained">
+          LinkComponent={Link} to="/showRecipe" sx={{ marginTop: 15, background: "#660066" }} variant="contained">
           <Typography variant="h4">View All Recipes</Typography>
         </Button>
       </Box>
